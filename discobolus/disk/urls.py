2ff14e9f3040fb31b20027e78efb5d34f35d951d
Pablo Klijnjan <pabloklijnjan@il.ibm.com>
2012-09-17 11:00:58 +0300

    Improved login button
        - Moved bootstrap icons to css/img (for some reason looks
          for them there)
        - Login button button more intuitive
        - Added sections to navbar

--- discobolus/disk/urls.py
+++ discobolus/disk/urls.py
@@ -7,7 +7,7 @@ from discobolus.disk.views import PartitionDetailView
 
 urlpatterns = patterns('discobolus.disk.views',
         url(r'list/$', login_required(DiskListView.as_view()), name='disk-list'),
-        url(r'^(?P<pk>\d+)/details/$', DiskDetailView.as_view(), name='disk-detail'),
-        url(r'^(?P<parent_pk>\d+)/partition/(?P<pk>\d+)/details/$', PartitionDetailView.as_view(), name='partition-detail'),
-        url(r'^multipath/(?P<pk>\d+)/details/$', MultipathDiskDetailView.as_view(), name='multipath-disk-detail'),
+        url(r'^(?P<pk>\d+)/details/$', login_required(DiskDetailView.as_view()), name='disk-detail'),
+        url(r'^(?P<parent_pk>\d+)/partition/(?P<pk>\d+)/details/$', login_required(PartitionDetailView.as_view()), name='partition-detail'),
+        url(r'^multipath/(?P<pk>\d+)/details/$', login_required(MultipathDiskDetailView.as_view()), name='multipath-disk-detail'),
         )
